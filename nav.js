15e7e25832245ce2e0225a18c31bd6a9a1a122b9
Marcos Pinheiro <Marcos.silva3@tramontina.com>
2026-03-14 20:13:22 -0300

    Atualização
    Adição do favicon e alteração no font-family do h3 das subsseos.

--- nav.js
+++ nav.js
@@ -1,14 +1,4 @@
-function clickmenu () {
-        if (itens.style.display == 'block'){
-            itens.style.display ='none'
-        } else {
-            itens.style.display='block' 
-        }
-        
-        
-    }
-
 function clicar () {
-    alert("Sua mensagem foi enviada !")   
+    alert("Sua mensagem foi enviada, por favor, atualize a página !")   
 }
 
