add31d04de5ff6c423e4216a09a2d0536d4eff30
marcospinheiroweb-bit <marcospinheiroweb@gmail.com>
2026-06-07 19:19:18 -0300

    add do pwa

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = "money-v1";
+
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/css/styles.css",
+  "/js/main.js",
+  "/favicon_io/android-chrome-192x192.png",
+  "/favicon_io/android-chrome-512x512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+});
